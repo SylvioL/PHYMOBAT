9a205cfeb807d8a2199ede9fba3a03c0d4f95f5d
SylvioL <sylvio.laventure@teledetection.fr>
2016-01-22 11:13:51 +0100

    Ajout de Seath.py

--- Seath.py
+++ Seath.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# This file is part of PHYMOBAT 1.0.
+# This file is part of PHYMOBAT 1.1.
 # Copyright 2016 Sylvio Laventure (IRSTEA - UMR TETIS)
 # 
-# PHYMOBAT 1.0 is free software: you can redistribute it and/or modify
+# PHYMOBAT 1.1 is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# PHYMOBAT 1.0 is distributed in the hope that it will be useful,
+# PHYMOBAT 1.1 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with PHYMOBAT 1.0.  If not, see <http://www.gnu.org/licenses/>.
+# along with PHYMOBAT 1.1.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy, math, sys
 import numpy as np # sort, ...
@@ -152,10 +152,10 @@ class Seath():
         for i in range(len(seuil)):
             if seuil[i] != "" and m1[i] > m2[i]:
                 print('For ' + ind  + ', the class 1 > ' + str(seuil[i]))
-                self.threshold.append('<' + str(seuil[i]))
+                self.threshold.append('>' + str(seuil[i]))
             elif seuil[i] != "" and m1[i] < m2[i]:
                 print('For ' + ind  + ', the class 1 < ' + str(seuil[i]))
-                self.threshold.append('>' + str(seuil[i]))
+                self.threshold.append('<' + str(seuil[i]))
             else:
                 print('For ' + ind  + ', not discrimination !')
                 sys.exit(1)

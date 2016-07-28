605159a54b55eb1fdccb5bda4597349e3aceb2bf
SylvioL <sylvio.laventure@gmail.com>
2016-07-28 16:20:30 +0200

    ADD:Add confusion matrix function from sklearn module

--- Toolbox.py
+++ Toolbox.py
@@ -20,6 +20,18 @@
 import os, subprocess
 import numpy as np
 
+from sklearn.metrics import confusion_matrix
+# Get all statistics precision, recall, f_score etc ...
+from sklearn.metrics import classification_report
+# Precision on the matrix diagonal
+from sklearn.metrics import accuracy_score
+# The precision is the ratio tp / (tp + fp) where tp is the number of true positives and fp the number of false positives
+from sklearn.metrics import recall_score
+# The precision is the ratio tp / (tp + fp) where tp is the number of true positives and fp the number of false positives.
+from sklearn.metrics import precision_score
+# The F1 score can be interpreted as a weighted average of the precision and recall F1 = 2 * (precision * recall) / (precision + recall)
+from sklearn.metrics import f1_score 
+
 def clip_raster(imag, vect):
     """
     Function to clip a raster with a vector. The raster created will be in the same folder than the input raster.
@@ -80,3 +92,111 @@ def calc_serie_stats(table):
         account_stats.append(i_stats.filled()) # Add stats table with true fill value
 
     return account_stats, account_cloud
+
+def cohen_kappa_score(y1, y2, labels=None):
+    """
+    Ref : https://github.com/scikit-learn/scikit-learn/blob/51a765a/sklearn/metrics/classification.py#L260 
+    
+    Cohen's kappa: a statistic that measures inter-annotator agreement.
+    This function computes Cohen's kappa [1], a score that expresses the level
+    of agreement between two annotators on a classification problem. It is
+    defined as
+    .. math::
+        \kappa = (p_o - p_e) / (1 - p_e)
+    where :math:`p_o` is the empirical probability of agreement on the label
+    assigned to any sample (the observed agreement ratio), and :math:`p_e` is
+    the expected agreement when both annotators assign labels randomly.
+    :math:`p_e` is estimated using a per-annotator empirical prior over the
+    class labels [2].
+    Parameters
+    ----------
+    y1 : array, shape = [n_samples]
+        Labels assigned by the first annotator.
+    y2 : array, shape = [n_samples]
+        Labels assigned by the second annotator. The kappa statistic is
+        symmetric, so swapping ``y1`` and ``y2`` doesn't change the value.
+    labels : array, shape = [n_classes], optional
+        List of labels to index the matrix. This may be used to select a
+        subset of labels. If None, all labels that appear at least once in
+        ``y1`` or ``y2`` are used.
+    Returns
+    -------
+    kappa : float
+        The kappa statistic, which is a number between -1 and 1. The maximum
+        value means complete agreement; zero or lower means chance agreement.
+    References
+    ----------
+    .. [1] J. Cohen (1960). "A coefficient of agreement for nominal scales".
+           Educational and Psychological Measurement 20(1):37-46.
+           doi:10.1177/001316446002000104.
+    .. [2] R. Artstein and M. Poesio (2008). "Inter-coder agreement for
+           computational linguistics". Computational Linguistic 34(4):555-596.
+    """
+    
+    confusion = confusion_matrix(y1, y2, labels=labels)
+    P = confusion / float(confusion.sum())
+    p_observed = np.trace(P)
+    p_expected = np.dot(P.sum(axis=0), P.sum(axis=1))
+    
+    return (p_observed - p_expected) / (1 - p_expected)
+
+def confus_matrix(data_class, data_val):
+    """
+    Function to compute ans save matrix confusion, precision, recall, 
+    f_scrore and overall accuracy of the classification. 
+    It compute all statistics because of 
+    """
+    
+    data_class = data_class.flatten() # to convert a matrix array in list array
+    data_val = data_val.flatten()
+    
+    # Add pixel value in a list without no data
+    fb_stats_2 = [[data_class[x],data_val[x]] for x in range(len(data_val)) if data_val[x] != -10000]
+    fb_stats_2 = map(list, zip(*fb_stats_2))# transpose list
+    fb_stats_in = fb_stats_2[0]
+    fb_stats_val = fb_stats_2[1]
+    
+    # Open a file (txt) to save results
+    f = open("ConfusionMatrix.txt", "wb")
+    
+    # Compute statistics on the classification
+    cm = confusion_matrix(fb_stats_val, fb_stats_in)
+    f.write("Confusion Matrix :\n")
+    for out in cm:
+        f.write(str(out) + "\n")
+    
+    all_pr = classification_report(fb_stats_val, fb_stats_in)
+    f.write("\n")
+    f.write(all_pr)    
+    
+    accur = accuracy_score(fb_stats_val, fb_stats_in)
+    f.write("\n")
+    f.write("Accuracy : " + str(accur) + "\n")
+    
+    recall = recall_score(fb_stats_val, fb_stats_in)
+    f.write("\n")
+    f.write("Recall : " + str(recall) + "\n")
+    
+    pr = precision_score(fb_stats_val, fb_stats_in)
+    f.write("\n")
+    f.write("Precision : " + str(pr) + "\n")
+    
+    f_scor = f1_score(fb_stats_val, fb_stats_in)
+    f.write("\n")
+    f.write("F_score : " + str(f_scor) + "\n")
+    
+    kappa = cohen_kappa_score(fb_stats_val, fb_stats_in)
+    f.write("\n")
+    f.write("Kappa : " + str(kappa) + "\n")
+    print('')
+    print('')
+    print('Accuracy : %f, Kappa : %f, Recall : %f, Precision : %f, F_score : %f' % (accur, kappa, recall, pr, f_scor))
+    print('')
+    print all_pr
+    print('')
+    print('Confusion matrix :')
+    print cm
+    
+    f.close()
+    
+    

b32060804d29c0691614321c3e2b614a6c53dc24
SylvioL <sylvio.laventure@gmail.com>
2017-02-03 11:29:20 +0100

    BUG: Add the choice in the layer_rasterization function to avoid MemoryError and create a zonal_statistics function to calculate percent majority with rasterstats module python

--- Vector.py
+++ Vector.py
@@ -145,20 +145,50 @@ class Vector():
             
         print('End of stats on ' + os.path.split(inraster)[1])
 
-    def layer_rasterization(self, raster_head, attribute_r):
+    def zonal_stats_pp(self, inraster):
         """
-        Function to rasterize a vector. Complete the gdal pointer empty properties with the layer's information
-        of the vector and a defined field.
+        A zonal statistics ++ to dertermine pxl percent in every polygon
+        
+        :param inraster: Input image path
+        :type inraster: str 
+        :returns: dict -- **p_stats** : dictionnary with pxl percent in every polygon. Mainly 'Maj_count' (Majority Value) and 'Maj_count_perc' (Majority Percent)
+        
+        """
+        
+        p_stats = raster_stats(str(self.vector_used), str(inraster), stats=['count'], copy_properties=True, categorical=True)
+        
+        for i in range(len(p_stats)):
+            percent = 0.0
+            for p in p_stats[i].keys():
+                if type(p) == np.float32 and p_stats[i][p]/float(p_stats[i]['count'])*100 > percent:
+                    p_stats[i]['Maj_count'] = p
+                    p_stats[i]['Maj_count_perc'] = p_stats[i][p]/float(p_stats[i]['count'])*100
+                    percent = p_stats[i]['Maj_count_perc']
+            if not 'Maj_count' in p_stats[i].keys() or not 'Maj_count_perc' in p_stats[i].keys():
+                p_stats[i]['Maj_count']=0
+                p_stats[i]['Maj_count_perc']=0
+        
+        return p_stats
+                
+    def layer_rasterization(self, raster_head, attribute_r, **kwargs):
+        """
+        Function to rasterize a vector. Define resolution, projection of the output raster with a raster head.
+        And complete the gdal pointer empty properties with the layer's information of the vector and a defined field.
+        If a raster has several band, in option you can choice if you want one band or more. 
         
         :param raster_head: Raster path that will look like the final raster of the rasterization
         :type raster_head: str
         :param attribute_r: Field name of the shapefile that contains class names
         :type attribute_r: str
+        :kwargs: **choice_nb_b** (int) - Output image number of band. If you choice 1, take first band. If you choice 2, take two first band etc... 
+        :returns: str -- **valid_raster** : output raster path from the rasterization
+        
         """
         
         # Export a example of a raster out information
         # for the validation shapefile
         example_raster = RasterSat_by_date('', '', [0]) # Call the raster class
+        example_raster.choice_nb_b = kwargs['choice_nb_b'] if kwargs.get('choice_nb_b') else 0
         raster_info = example_raster.raster_data(raster_head)# Extract data info
         
         # Define the validation's vector
@@ -178,11 +208,12 @@ class Vector():
         # Virtual rasterize the vector 
         pt_rast = gdal.RasterizeLayer(self.raster_ds, [1], self.data_source.GetLayer(), \
                                       options=["ATTRIBUTE=" + str(attribute_r)])
+        
         if pt_rast != 0:
             raise Exception("error rasterizing layer: %s" % pt_rast)
         
         new_data = self.raster_ds.ReadAsArray()
-               
+        
         self.raster_ds = None
         # Complete the raster creation
         example_raster.complete_raster(info_out, new_data)
